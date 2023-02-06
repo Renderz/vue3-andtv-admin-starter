f3b7e89036c80ea3ba69edad4d06494164832906
Renderz <464816158@qq.com>
2023-02-06 14:21:17 +0800

    feat: add env files

--- types/vite.d.ts
+++ types/vite.d.ts
@@ -2,8 +2,9 @@ declare interface ViteEnv {
   VITE_PORT: number;
   VITE_PUBLIC_PATH: string;
   VITE_PROXY: [string, string][];
-  VITE_GLOB_APP_TITLE: string;
+  VITE_APP_TITLE: string;
   VITE_DROP_CONSOLE: boolean;
   VITE_LEGACY: boolean;
   VITE_HTTPS: boolean;
+  VITE_API_URL: string;
 }
